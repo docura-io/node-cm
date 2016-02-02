e757eebe961382b87cd19dc7528d00730c461f38
tucaz <me@tucaz.net>
2016-02-02 08:44:31 -0600

    Added start method

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 module.exports = new (function (proc, psFind) {
     var self = this,
         _cm = null,
-        env_vars_set = false;
+        _cmExecutable = "_cm.exe",
+        env_vars_set = false,
+        _onReady = null,
+        _onRead = null,
+        _onError = null,
+        _onExit = null;
 
-    this.start = function (cm_path, readyCallback) {
-        self.setEnvironmentVariables();
-        self.kill();
+    this.setup = function (onReady, onRead, onError, onExit) {
+        _onReady = onReady;
+        _onRead = onRead;
+        _onError = onError;
+        _onExit = onExit;
+    };
 
-        _cm = proc.spawn(cm_path, ["/develop", "/nocoloring", "/clean"]);
+    this.start = function (cm_path) {
+        return new Promise(function (resolve, reject) {
+            if (cm_path) {
+                _cmExecutable = cm_path;
+            }
 
-        _cm.stdout.on("data", function (data) {
-            console.log("stdout", data.toString());
-        });
+            self.setEnvironmentVariables();
+            self.kill();
 
-        _cm.stderr.on("data", function (data) {
-            console.log("stderr", data.toString());
-        });
+            _cm = proc.spawn(_cmExecutable, ["/develop", "/nocoloring"]);
 
-        _cm.on("exit", function (code) {
-            console.log("Child exited with code", code);
-        });
-        
-        setTimeout(function () {
-            console.log("sending command");
-            _cm.stdin.write("pln(\"Hello\");\x01");
-        }, 10000);
-        
-        return new Promise(function(resolve, reject){
-            reject();    
+            _cm.stdout.on("data", function (data) {
+                if (_onRead)
+                    _onRead(data.toString());
+
+                if (data.indexOf("startup done in") > -1) {
+                    resolve(true);
+                }
+            });
+
+            _cm.stderr.on("data", function (data) {
+                if (_onError)
+                    _onError(data.toString());
+                else
+                    throw new Error(data.toString());
+            });
+
+            _cm.on("exit", function (code) {
+                if (_onExit)
+                    _onExit(code);
+            });
         });
     };
 
-    this.kill = function() {
+    this.kill = function () {
         self.setEnvironmentVariables();
 
         console.log("Killing pending processes");
@@ -46,19 +64,21 @@ module.exports = new (function (proc, psFind) {
         } else {
             env_vars_set = true;
         }
-        
-        var CET_DEV = "C:\\CetDev\\version6.5\\";
+
+        var CM_ROOT = "C:\\CetDev\\version6.5",
+            CM_HOME = CM_ROOT + "\\home",
+            CM_WRITE = CM_ROOT + "\\write";
 
         process.env["CM_ARCH"] = "win64";
-        process.env["CM_ENVFILE_EMACS"] = CET_DEV + "write\\_emacs.cmenv";
-        process.env["CM_ENVFILE_OPERATOR"] = CET_DEV + "write\\_operator.cmenv";
-        process.env["CM_HOME"] = CET_DEV + "home";
-        process.env["CM_LIBS"] = CET_DEV + "write\\data\\cm-libs\\win64";
-        process.env["CM_ROOT"] = CET_DEV;
-        process.env["CM_UNIX_HOME"] = "C:/CetDev/version6.5/home";
-        process.env["CM_UNIX_WRITE"] = "C:/CetDev/version6.5/write";
+        process.env["CM_ENVFILE_EMACS"] = CM_WRITE + "\\_emacs.cmenv";
+        process.env["CM_ENVFILE_OPERATOR"] = CM_WRITE + "\\_operator.cmenv";
+        process.env["CM_HOME"] = CM_HOME;
+        process.env["CM_LIBS"] = CM_WRITE + "\\data\\cm-libs\\win64";
+        process.env["CM_ROOT"] = CM_ROOT;
+        process.env["CM_UNIX_HOME"] = CM_HOME;
+        process.env["CM_UNIX_WRITE"] = CM_WRITE;
         process.env["CM_VCVERSION"] = "10";
-        process.env["CM_WRITE"] = "C:\\CetDev\\version6.5\\write";
-        process.env["PATH"] = process.env["PATH"] + ";" + process.env["CM_HOME"] + "\\bin\\";
+        process.env["CM_WRITE"] = CM_WRITE;
+        process.env["PATH"] = process.env["PATH"] + ";" + CM_HOME + "\\bin\\;" + CM_HOME + "\\bin\\win64;";
     }
 })(require("child_process"), require("ps-node"));
--- tests/compilerTests.js
+++ tests/compilerTests.js
@@ -5,15 +5,25 @@ var mocha = require("mocha"),
 describe("compiler", function () {
     it("should start CM", function (done) {
         this.timeout(40 * 1000);
-        cm.start();
+        var onStart = cm.start();
         
-        // onStart.then(done, done).catch(done);
+        onStart.then(function(success) { if(success) done(); }, done).catch(done);
     });
     
-//     it("should set environment variables", function () {
-//         cm.setEnvironmentVariables();
-//         process.env["CM_ARCH"].should.be.equal("win64");
-//     });
+    // it("should set environment variables", function () {
+    //     cm.setEnvironmentVariables();
+    //     
+    //     process.env["CM_ARCH"].should.be.equal("win64");
+    //     process.env["CM_ENVFILE_EMACS"].should.be.equal("C:\\CetDev\\version6.5\\write\\_emacs.cmenv");
+    //     process.env["CM_ENVFILE_OPERATOR"].should.be.equal("C:\\CetDev\\version6.5\\write\\_operator.cmenv");
+    //     process.env["CM_HOME"].should.be.equal("C:\\CetDev\\version6.5\\home");
+    //     process.env["CM_LIBS"].should.be.equal("C:\\CetDev\\version6.5\\write\\data\\cm-libs\\win64");
+    //     process.env["CM_ROOT"].should.be.equal("C:\\CetDev\\version6.5");
+    //     process.env["CM_UNIX_HOME"].should.be.equal("C:\\CetDev\\version6.5\\home");
+    //     process.env["CM_UNIX_WRITE"].should.be.equal("C:\\CetDev\\version6.5\\write");
+    //     process.env["CM_VCVERSION"].should.be.equal("10");
+    //     process.env["CM_WRITE"].should.be.equal("C:\\CetDev\\version6.5\\write");
+    // });
 // 
 //     it("should kill CM", function () {
 //         var code = cm.kill();
